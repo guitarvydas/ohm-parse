3fd0215914db054223a5c7df8829c32c7a6059fc
Paul Tarvydas <guitarvydas@gmail.com>
2022-01-17 16:57:10 -0500

    regression test

--- run.bash
+++ run.bash
@@ -1,2 +1,6 @@
 prep=../prep/prep
-$prep '.' '$' ohm.ohm ohm.glue --stop=1 <ohm.ohm
+$prep '.' '$' ohm.ohm ohm.glue --stop=1 <ohm.ohm >regression.ohm
+tr '\n' ' ' <ohm.ohm >temp.ohm
+tr '\n' ' ' <regression.ohm >temp2.ohm
+echo '***' diffing '***'
+diff -w temp.ohm temp2.ohm
